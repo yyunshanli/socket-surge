830d94131bf0d5ec17f27c21bdf86b12ee868750
yunshan <156245615+yyunshanli@users.noreply.github.com>
2024-10-23 20:54:12 -0700

    compiler

---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'katex/dist/katex.min.css'; // Import the CSS file
+import { InlineMath, BlockMath } from 'react-katex'; 
+
+
+function App() {
+  const [String,setString] = useState('');
+  const [count,setCount] = useState(0);
+  return (
+    
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <input type="text" onChange={(e)=>{
+        setString(e.target.value)
+      }
+      }/>
+      <InlineMath math={String}></InlineMath>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
